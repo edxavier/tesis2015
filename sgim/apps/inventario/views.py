b1e2d6371cced8df158a7dc3bb7113d892b58098
Eder Xavier Rojas <edxavier05@gmail.com>
2015-04-16 20:56:25 -0600

    Login Con modelo personalisado

--- sgim/apps/inventario/views.py
+++ sgim/apps/inventario/views.py
@@ -1,3 +1,13 @@
+# Create your views here.
 from django.shortcuts import render
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.views.generic import View
+from django.template.context import RequestContext
 
 # Create your views here.
+
+# Create your views here.
+class NuevoDispositivo(View):
+    def get(self, request, *args, **kwargs):
+        return render_to_response('inventario/nuevo_dispositivo.html',
+            locals(), context_instance=RequestContext(request))
--- sgim/sgim/urls.py
+++ sgim/sgim/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
                         url(r'^cuentas/',include('apps.cuentas.urls')),
                         url(r'^mantto/',include('apps.mantenimiento.urls')),
+                        url(r'^inventario/',include('apps.inventario.urls')),
 
 
                         url(r'^admin/', include(admin.site.urls)),
