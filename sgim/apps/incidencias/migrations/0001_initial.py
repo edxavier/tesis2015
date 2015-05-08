ed89b52018fd8f92bffb2fb4e6c93bbc087ce7ea
Eder Xavier Rojas <edxavier05@gmail.com>
2015-05-07 21:41:15 -0600

    Creando formulario para ingreso de datos usando ajax

--- sgim/apps/incidencias/migrations/0001_initial.py
+++ sgim/apps/incidencias/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('inventario', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('catalogo', '0002_auto_20150507_0824'),
+        ('catalogo', '0002_auto_20150507_2104'),
     ]
 
     operations = [
