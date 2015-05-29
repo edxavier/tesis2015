b1e3961a92718b3e25b9f6ebbf4d7ca516f7737e
edxavier <edxavier05@gmail.com>
2015-05-29 12:35:30 -0600

    Socket IO

---
+++ sgim/static/js/ioMain.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+    io = io.connect('http://192.168.137.85:8500');
+    //io = io.connect('http://127.0.0.1:8500');
+    io.on('connect', function(){
+                    console.log('Connected to server');
+                });
+    var notifications = 0
+	var noti_boot = 0
+	var noti_interface = 0
+	var noti_general = 0
+    
+    io.on('boot_event', function(data){
+		if(data.tipo==="nsNotifyShutdown"){
+        	$.snackbar({content: "<i class='ion-power red icon'></i> "+data.direccion+" se ha apagado",timeout: 10000});
+            var audio = new Audio('/static/snd/shutdown.wav');
+            audio.play();
+        }
+        else{
+        	$.snackbar({content: "<i class='ion-power green icon'></i> "+data.direccion+" ha iniciado",timeout: 10000});
+            var audio = new Audio('/static/snd/startup.mp3');
+            audio.play();
+        }
+
+        notifications += 1
+        noti_boot += 1
+        $('.gestion_menu').html(notifications)
+        $('.gestion_boot').html(noti_boot)
+	});
+    
+    
+    
+    io.on('interface_event', function(data){
+		notifications += 1
+        noti_interface += 1
+           var audio = new Audio('/static/snd/pop.mp3');
+        audio.play();
+        $('.gestion_menu').html(notifications)
+        $('.gestion_interface').html(noti_interface)
+        if(data.tipo==="linkUp")
+            $.snackbar({content: data.direccion+": "+data.nombre+" <i class='ion-arrow-up-a green icon'></i>",timeout: 10000});
+        else
+            $.snackbar({content:data.direccion+": "+data.nombre+ " <i class='ion-arrow-down-a orange icon'></i>",timeout: 10000});
+
+	});
+    io.on('general_event', function(data){
+		console.log(data);
+		notifications += 1
+        noti_general += 1
+        var audio = new Audio('/static/snd/pop.mp3');
+        audio.play();
+        if(data.warning === true || data.warning === "True")
+            $.snackbar({content: data.direccion+": "+data.item+" <i class='ion-ios-gear red	 icon'></i>",timeout: 10000});
+        else
+            $.snackbar({content: data.direccion+": "+data.item+" <i class='ion-ios-gear green icon'></i>",timeout: 10000});
+
+        $('.gestion_menu').html(notifications)
+        $('.gestion_general').html(noti_general)
+	});
+    
+    io.on('heart_beat', function(data){
+		//console.log(data)
+        if(data.esta_conectado==="True"){
+            $.snackbar({content: data.direccion+": Conexion Reestablecida<i class='ion-arrow-swap green icon'></i>",timeout: 10000});
+            var audio = new Audio('/static/snd/noty2.mp3');
+            audio.play();
+        }else{
+            $.snackbar({content:data.direccion+":  Conexion Perdida <i class='ion-arrow-swap orange icon'></i>",timeout: 10000});
+            var audio = new Audio('/static/snd/noty1.mp3');
+            audio.play();
+        }
+	});
+
+    
+})
