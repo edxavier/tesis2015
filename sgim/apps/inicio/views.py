36249ec6b1741af48ce05bb26986928eb134dd07
Eder Xavier Rojas <edxavier05@gmail.com>
2015-06-06 21:14:17 -0600

    Pagina de inicio

--- sgim/apps/inicio/views.py
+++ sgim/apps/inicio/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db.models.aggregates import Count
 from django.shortcuts import render_to_response
 from django.views.generic import TemplateView
@@ -9,10 +10,19 @@ from apps.mantenimiento.models import Programacion
 
 class Home(TemplateView):
     def get(self, request):
-        inc_pendiente = Incidencia.objects.all().exclude(estado_id=1).count()
-        mantto_programado = Programacion.objects.filter(estado_id=1).count()
-        mantto_progreso = Programacion.objects.filter(estado_id=2).count()
-        camb_programado = Cambio.objects.filter(estado_id=1).count()
+        today = datetime.now()
+        num_incidencias = Incidencia.objects.filter(creado__month=today.month, creado__year=today.year).count()
+        mantto_programado = Programacion.objects.filter(creado__month=today.month, creado__year=today.year).count()
+        mantto_progreso = Programacion.objects.filter(creado__month=today.month, creado__year=today.year, estado_id=2).count()
+        mantto_terminado = Programacion.objects.filter(creado__month=today.month, creado__year=today.year, estado_id=4).count()
+
+        incidentes = Incidencia.objects.all().exclude(estado_id=1)[:15]
+        manttos = Programacion.objects.filter(estado_id=1)[:15]
+        cambios = Cambio.objects.filter(estado_id=1)[:15]
+        inc_cerradas = Incidencia.objects.filter(creado__month=today.month, creado__year=today.year, estado_id=1).count()
+        tasa_resolucion = (inc_cerradas * 100) / num_incidencias
+        tasa_cumplimiento = (mantto_terminado * 100) / mantto_programado
+        tasa_progreso = (mantto_progreso * 100) / mantto_programado
 
         return render_to_response(
             'home.html', locals(), context_instance=RequestContext(request)
