fc5eab1e6eb95606d18c5ca3dd793faed95723fb
edxavier <edxavier05@gmail.com>
2015-06-12 12:38:24 -0600

    Casi termindao

--- sgim/apps/mantenimiento/statistic_view.py
+++ sgim/apps/mantenimiento/statistic_view.py
@@ -67,4 +67,37 @@ class MonthlyStatisticJson(View):
 class YearlyStatisticJson(View):
     def get(self, request, *args, **kwargs):
         today = datetime.now()
-        pass
+
+        mantt_user = (Programacion.objects.values('responsable__username')
+                    .annotate(total=Count('id'))
+                    .filter( creado__year=today.year)
+                    .order_by('-total'))
+
+        mantt_equipo = (BoletaTrabajo.objects.values('dispositivo__posicion_logica')
+                    .annotate(total=Count('id'))
+                    .filter( creado__year=today.year)
+                    .order_by('-total'))
+
+        paro_equipo = (BoletaTrabajo.objects.values('dispositivo__posicion_logica')
+                    .annotate(total=Avg('tiempo_paro'))
+                    .filter(creado__year=today.year, paro_operacion=True)
+                    .order_by('-total'))
+
+        manttos_user = []
+        manttos_estado = []
+        avg_paro = []
+
+        for e in mantt_user:
+             manttos_user.append((Statistics(pos=e['responsable__username'], total=e['total'])))
+        for e in mantt_equipo:
+             manttos_estado.append((Statistics(pos=e['dispositivo__posicion_logica'], total=e['total'])))
+        for e in paro_equipo:
+             avg_paro.append((Statistics(pos=e['dispositivo__posicion_logica'], total=e['total'])))
+
+        serial_mantto_user = StatisticSerializer(manttos_user, many=True)
+        serial_mantto_estado = StatisticSerializer(manttos_estado, many=True)
+        serial_avg_paro = StatisticSerializer(avg_paro, many=True)
+        return JsonResponse({'success': True, 'asignaciones':serial_mantto_user.data,
+                             'trabajo_equipos':serial_mantto_estado.data, 'inactividad_avg': serial_avg_paro.data
+                             })
+
