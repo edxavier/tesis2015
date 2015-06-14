9b2be276c987ddb1cafb67e875f7500f5c354059
edxavier <edxavier05@gmail.com>
2015-06-14 12:20:17 -0600

    Casi termindao

--- sgim/apps/inicio/middlewares.py
+++ sgim/apps/inicio/middlewares.py
@@ -5,7 +5,10 @@ from django.http import HttpResponseRedirect
 class LoginRedirectMiddleware(object):
     """docstring for AjaxResponse"""
     def process_request(self, request):
-        if request.path == "/login/" and request.user.is_authenticated():
-            return HttpResponseRedirect("/")
-        elif request.path == "/admin/" and not request.user.is_admin():
+        if request.user.is_authenticated():
+            if request.path == "/login/":
+                return HttpResponseRedirect("/")
+            elif request.path == "/admin/" and not request.user.is_admin():
+                return HttpResponseRedirect("/")
+        else:
             return HttpResponseRedirect("/")
