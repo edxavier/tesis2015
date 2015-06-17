3dc410378e0a7cebfc5b742f7356a16b45f82eb5
edxavier <edxavier05@gmail.com>
2015-06-17 10:55:37 -0600

    Casi termindao

--- NMS/https.js
+++ NMS/https.js
@@ -23,48 +23,48 @@ app.get('/', function (req, res) {
 });
 
 
-server.post('/boot_event/', function (req, res) {
+app.post('/boot_event/', function (req, res) {
 
    // console.info(req.body)
-    server.io.broadcast("boot_event", req.body)
+    app.io.broadcast("boot_event", req.body)
     res.send("Boot Event Received");
 });
 
-server.post('/interface_event/', function (req, res) {
+app.post('/interface_event/', function (req, res) {
 
    // console.info(req.body)
-    server.io.broadcast("interface_event", req.body)
+    app.io.broadcast("interface_event", req.body)
     res.send("INTERFACE Event Received");
 });
 
 
-server.post('/general_event/', function (req, res) {
-    server.io.broadcast("general_event", req.body)
+app.post('/general_event/', function (req, res) {
+    app.io.broadcast("general_event", req.body)
     res.send("GENERAL Event Received");
 });
 
-server.post('/host_update/', function (req, res) {
-    server.io.broadcast("host_update", req.body)
+app.post('/host_update/', function (req, res) {
+    app.io.broadcast("host_update", req.body)
     //console.info(req.body)
     //console.info("UPDATE HOST")
     res.send("success");
 });
 
 
-server.post('/heart_beat/', function (req, res) {
-    server.io.broadcast("heart_beat", req.body)
+app.post('/heart_beat/', function (req, res) {
+    app.io.broadcast("heart_beat", req.body)
     //console.info(req.body)
     //console.info("Heart Beat")
     res.send("success");
 });
 
-server.post('/mem_usage/', function (req, res) {
-    server.io.broadcast("mem_usage", req.body)
+app.post('/mem_usage/', function (req, res) {
+    app.io.broadcast("mem_usage", req.body)
     res.send("success");
 });
 
-server.post('/load_avg/', function (req, res) {
-    server.io.broadcast("load_avg", req.body)
+app.post('/load_avg/', function (req, res) {
+    app.io.broadcast("load_avg", req.body)
     res.send("success");
 });
 
