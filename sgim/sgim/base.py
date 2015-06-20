da60c2e8eacb9b530013482ebade99793d155dc7
Eder Xavier Rojas <edxavier05@gmail.com>
2015-06-20 13:49:15 -0600

    Socket IO

--- sgim/sgim/base.py
+++ sgim/sgim/base.py
@@ -95,6 +95,32 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStora
 ROOT_URLCONF = 'sgim.urls'
 WSGI_APPLICATION = 'sgim.wsgi.application'
 
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
